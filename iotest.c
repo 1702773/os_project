fa7b1338fce9dd54cf5bc39d42880a206557f84d
TankLee <1702773@gmail.com>
2022-04-18 10:28:43 +0800

    20220418

--- iotest.c
+++ iotest.c
@@ -3,26 +3,82 @@
 #include <string.h>
 #include <unistd.h>
 #include <fcntl.h>
-
+#include "tankfunction.h"
 int iotest(char userinput[4][1024]){
     printf("iotest start-----------------\n");
         for(int i = 0 ; i < 4;i++){
         printf("Your %d input is:%s\n",i,userinput[i]);
     }
     //先判斷是輸入還是輸出,先以可以讓ls輸出到一個txt檔案為目標
-    if(strcmp(userinput[1],"<<")==0){
+    for(int i = 0 ; i < 4;i++){
+        pid_t child;
+    if(strcmp(userinput[i],"<<")==0){
         printf("input\n");
-    }else if(strcmp(userinput[1],">>")==0){
+        char ioinput[4][1024] = {' '};
+        //userinput[i]之後的為檔案名稱 
+        
+        char iofile[1024] = {' '};
+        strcpy(iofile,userinput[i+1]);
+        //userinput[i]之前為指令
+        if(strcmp(userinput[i-1],"")==0){
+            printf("Please reinput\n");
+            break;
+        }else{
+            if(strcmp(userinput[i+1],"")==0){
+                printf("Please input what file do you want input\n");
+                break;               
+            }else{
+                for(int k = 0;k<i;k++){
+                    strcpy(ioinput[k],userinput[k]);
+                    printf("ioinput array %d is:%s\n",k,ioinput[k]);
+                }
+            }
+            open(iofile,O_RDWR|O_CREAT,S_IRUSR|S_IWUSR);
+            //close(1);
+            //open(iofile,O_RDWR|O_CREAT,S_IRUSR|S_IWUSR);
+            //printf("This is a test\n");
+            //normalexec(ioinput);
+            //userinput[i]之前的為指令
+            //normalexec(userinput);
+            //int fd =open("/dev/tty",O_WRONLY);
+            //stdout = fdopen(fd,"w");
+        }
+        //將檔案內容導入到指令
+        break;
+    }else if(strcmp(userinput[i],">>")==0){
         printf("output\n");
         printf("ls output test\n");
-        close(1);
-        open("./output.txt", O_WRONLY | O_CREAT );
-        printf("asasdsdsdsdsd");
-        fclose(fp);
+        char ioinput[4][1024] = {' '};
+        //userinput[i]之後的為檔案名稱 
+        char iofile[1024] = {' '};
+        strcpy(iofile,userinput[i+1]);
+        //printf("%s\n",userinput[i+1]);
+        if(strcmp(userinput[i+1],"")==0){
+            printf("Please input what file you want input\n");
+            break;
+        }else{
+            for(int k = 0;k<i;k++){
+                strcpy(ioinput[k],userinput[k]);
+                printf("ioinput array %d is:%s\n",k,ioinput[k]);
+            }
+            close(1);
+            open(iofile,O_RDWR|O_CREAT,S_IRUSR|S_IWUSR);
+            //printf("This is a test\n");
+            normalexec(ioinput);
+            //userinput[i]之前的為指令
+            //normalexec(userinput);
+            int fd =open("/dev/tty",O_WRONLY);
+            stdout = fdopen(fd,"w");
+        }
+        //open("./output.txt", O_RDWR | O_CREAT ,S_IRUSR | S_IWUSR);
+        //dup(1);   
+        printf("io output finish\n");
+        break;
     }else{
-        printf("ioerror\n");
+        
     }
-    printf("iotest end-----------------\n"); 
+    printf("iotest end-----------------\n");
+    }
     return 0;
 
 }
